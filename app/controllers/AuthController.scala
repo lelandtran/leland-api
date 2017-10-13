bdb8aeef46f1078fcda5f4afd0186feb28d93785
Leland Tran <lelandtran@ymail.com>
2017-10-12 20:37:55 -0700

    Successfully retrieve OAuth 2 access token

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -2,24 +2,29 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.WSClient
 import play.api.mvc.{AbstractController, ControllerComponents}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class AuthController @Inject() (cc: ControllerComponents, ws: WSClient) extends AbstractController(cc){
+class AuthController @Inject() (cc: ControllerComponents, configuration: play.api.Configuration, ws: WSClient) extends AbstractController(cc){
 
-  def authGithubCb(code: String) = Action { request =>
+  val clientId = configuration.underlying.getString("auth.github.client_id")
+  val clientSecret = configuration.underlying.getString("auth.github.client_secret")
+
+  def authGithubCb(code: String) = Action.async { request =>
     ws.url("https://github.com/login/oauth/access_token").post(Json.obj(
       "code" -> code,
-      "client_id" -> "CLIENT_ID",
-      "client_secret" -> "CLIENT_SECRET")).map { response =>
-      System.out.println(response)
-      Ok("Sent request with code " + code)
-      // this secondary async response does not work;
-      // should check if one response to one request is not just a limitation in java, but the protocol
+      "client_id" -> clientId,
+      "client_secret" -> clientSecret))
+      .map { response =>
+        val body = response.body
+        val status = response.status
+        Ok("Client Id: "+clientId+".\n" +
+          "Sent request with code " + code + ".\n" +
+          "Got response " + status + ": " + body)
     }
-    Ok("Sent access token request with code " + code + ", redirecting...")
   }
 }
