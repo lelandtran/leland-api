f68ec4b1e2e68e6a0ccab902f459628ceb27dc4e
Leland <lelandtran@ymail.com>
2018-08-26 22:55:52 -0700

    Add RedisClient connection to local

--- app/v1/routingnums/RoutingNumbersController.scala
+++ app/v1/routingnums/RoutingNumbersController.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.Future
 
 import scala.concurrent.duration._
 import akka.actor.ActorSystem
+import com.redis.RedisClient
 import javax.inject.Inject
 import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, BaseController, ControllerComponents, DefaultControllerComponents}
@@ -14,10 +15,16 @@ import scala.concurrent.duration.FiniteDuration
 class RoutingNumbersController @Inject()(cc: DefaultControllerComponents, ws: WSClient)(implicit ec: ExecutionContext) extends BaseController {
   override protected def controllerComponents: ControllerComponents = cc
 
+  val rc = new RedisClient("localhost", 6379)
+
   def show(routingNumber: String) = Action.async{ implicit request =>
     ws.url("https://www.routingnumbers.info/api/data.json")
       .addQueryStringParameters(("rn", routingNumber))
       .get()
-      .map(resp => Ok(resp.body))
+      .map(resp => {
+        rc.incrby("routingNumber:"+routingNumber+":reqs", 1)
+          .map(reqs => (resp, reqs))
+      })
+      .map(respAndReqs => Ok("Req count for " + routingNumber + ":" +respAndReqs.get._2 + "\n resp:"+respAndReqs.get._1.body))
   }
 }
